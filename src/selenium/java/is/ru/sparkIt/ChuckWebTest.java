8926ced7bcf05a488987997e008ffcf99f0c6bac
Skúli Arnlaugsson <skuli.arnlaugsson@gmail.com>
2014-10-23 21:38:12 +0000

    Refactored selenium test setup and added a test
    For getting a specific joke by id.

--- src/selenium/java/is/ru/sparkIt/ChuckWebTest.java
+++ src/selenium/java/is/ru/sparkIt/ChuckWebTest.java
@@ -1,5 +1,8 @@
 package is.ru.sparkit;
 
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -8,16 +11,25 @@ import org.junit.Test;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.*;
 import org.openqa.selenium.WebDriver;
 
 public class ChuckWebTest {
 
     static WebDriver driver;
+    static String baseUrl;
+    static String port;
 
     @BeforeClass
     public static void before() {
         // Will be run before all tests in class are run
         driver = new FirefoxDriver();
+        port = System.getenv("PORT");
+        if(port == null) {
+            port = "4567";
+        }
+        baseUrl = "http://localhost:" + port;
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     }
 
     @AfterClass
@@ -33,13 +45,21 @@ public class ChuckWebTest {
     }
 
     @Test
-    public void assertNameInJoke() {
-        String port = System.getenv("PORT");
-        if(port == null){
-            port = "4567";
-        }
-        driver.get("http://localhost:" + port);
-        String bodyText = driver.findElement(By.tagName("body")).getText();
-        assertTrue(bodyText.toLowerCase().contains("chuck norris"));
+    public void assertTitle() {
+        driver.get(baseUrl + "/");
+        assertEquals("Chuck Norris Jokes", driver.getTitle());
+    }
+
+    @Test
+    public void assertSpecificJoke() {
+        driver.get(baseUrl + "/specific.html");
+        driver.findElement(By.id("number")).clear();
+        driver.findElement(By.id("number")).sendKeys("5");
+        driver.findElement(By.cssSelector("button[type=\"submit\"]")).click();
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("alert")));
+        String expectedJoke = "Chuck Norris lost his virginity before his dad did.";
+        String foundJoke = driver.findElement(By.id("results")).getText();
+        assertEquals(foundJoke, expectedJoke);
     }
 }
